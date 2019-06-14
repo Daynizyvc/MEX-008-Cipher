9672cdce6105e236117b2824640811e97a4b6e3b
Daynizyvc <zinyad_leay@hotmail.com>
2019-06-14 12:40:28 -0500

    Funciones que ya pasaron los tests

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,25 @@
 window.cipher = {
-  // ... 
-};
+  encode : (n , str) =>{
+    const newLetters = [ ];
+     for(let i = 0; i < str.length; i++){
+       const letter = str.charCodeAt(i);
+       const newASCII = (letter-65+n)%26+65;
+       newLetters.push(String.fromCharCode(newASCII)); 
+    }
+    const newStr = newLetters.join('');
+    return newStr;
+   },
+
+    decode : (nA , strA) => {
+     const textoOriginal = [];
+      for(let i=0; i < strA.length; i++){
+        const textoAscii= strA.charCodeAt(i);
+        const asciiNew = (textoAscii+65-nA)%26+65;
+        textoOriginal.push(String.fromCharCode(asciiNew));
+      }
+      const textoMostrar = textoOriginal.join('');
+      return textoMostrar;
+  }
+}
+
+
