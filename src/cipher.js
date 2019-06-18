b2dfb3318c46814943a9a9802214e3622b6d851b
Daynizyvc <zinyad_leay@hotmail.com>
2019-06-18 17:07:32 -0500

    El proyecto cifra y descifra en mayúsculas y minúsculas, y algunos caracteres y el espacio solo los devuelve

--- src/cipher.js
+++ src/cipher.js
@@ -9,24 +9,37 @@ window.cipher = {
     }else if(letter >= 97 && letter <= 122){
         const newASCII = (letter - 97 + n) % 26 + 97;
         newLetters.push(String.fromCharCode(newASCII));
-    }else if (letter == '32'){
+      }else if (letter >= 32 && letter <= 64 || letter >= 91 && letter <= 96 || letter >= 123 && letter <= 165 || letter == 168){
         newLetters.push(String.fromCharCode(letter));
-    };  
+      };
     };
     const newStr = newLetters.join('');
     return newStr;
    },
 
     decode : (nA , strA) => {
-     const textoOriginal = [];
-      for(let i=0; i < strA.length; i++){
-        const textoAscii= strA.charCodeAt(i);
-        const asciiNew = (textoAscii+65-nA)%26+65;
-        textoOriginal.push(String.fromCharCode(asciiNew));
-      }
+      const textoOriginal = [];
+      for (let i = 0; i < strA.length; i++) {
+        const textoAscii = strA.charCodeAt(i);
+        if (textoAscii >= 65 && textoAscii <= 90) {
+         let asciiA = ((textoAscii - 65 - nA) % 26);
+          if (asciiA < 0) {
+            asciiA = asciiA + 26;
+          };
+          asciiA = asciiA + 65;
+          textoOriginal.push(String.fromCharCode(asciiA));
+        } else if (textoAscii >= 97 && textoAscii <= 122) {
+        let asciiA = ((textoAscii - 97 - nA) % 26);
+          if (asciiA < 0) {
+          asciiA = asciiA + 26;
+          };
+          asciiA = asciiA + 97;
+          textoOriginal.push(String.fromCharCode(asciiA));
+        }else if (textoAscii >= 32 && textoAscii <= 64 ||textoAscii >= 91 && textoAscii <= 96 || textoAscii >= 123 && letter <= 169){
+          textoOriginal.push(String.fromCharCode(textoAscii));
+        };
+      };
       const textoMostrar = textoOriginal.join('');
       return textoMostrar;
-  }
+  },
 }
-
-
